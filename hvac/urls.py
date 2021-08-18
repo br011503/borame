65c294148681b0668833ac3537ab07b2467c864f
br011503 <49256179+br011503@users.noreply.github.com>
2021-08-18 21:16:26 +0900

    Update urls.py

--- hvac/urls.py
+++ hvac/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [    
 #     path('', views.hello, name='hello'),
