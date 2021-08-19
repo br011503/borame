babca29967bb7ac09976c50a288133b9095900da
br011503 <49256179+br011503@users.noreply.github.com>
2021-08-19 09:13:49 +0900

    Update settings.py

--- django_hvac/settings.py
+++ django_hvac/settings.py
@@ -117,6 +117,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+STATIC_URL = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
