e146c4d14a2719c779ad5a8a7832f7361aee4559
br011503 <49256179+br011503@users.noreply.github.com>
2021-08-19 13:36:55 +0900

    Update settings.py

--- django_hvac/settings.py
+++ django_hvac/settings.py
@@ -119,3 +119,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = ( os.path.join('static'), )
+STATICFILES_FINDERS = ('django.contrib.staticfiles.finders.FileSystemFinder',
+                       'django.contrib.staticfiles.finders.AppDirectoriesFinder',)
