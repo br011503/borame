afec4a50cab0343cd8c7612dfd1062f6163fd239
br011503 <49256179+br011503@users.noreply.github.com>
2021-08-19 12:26:09 +0900

    Update urls.py

--- hvac/urls.py
+++ hvac/urls.py
@@ -4,4 +4,4 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [ url(r'^$', views.index, name='index'), ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = [ url(r'^$', views.index, name='index'), ] + static(settings.STATIC_URL, document_root=settings.STATIC_DIR)
