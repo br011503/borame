14262b277007993169b02d55a07626655070bf36
br011503 <49256179+br011503@users.noreply.github.com>
2021-08-20 13:37:54 +0900

    Update urls.py

--- hvac/urls.py
+++ hvac/urls.py
@@ -5,9 +5,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [ url(r'^$', views.index, name='index'), 
-                url(r'^$', views.page_hvac, name='page_hvac'), 
-                url(r'^$', views.page_temp, name='page_temp'), 
-                url(r'^$', views.page_cool, name='page_cool'), 
-                url(r'^$', views.page_elec, name='page_elec'), 
-                url(r'^$', views.page_peak, name='page_peak'), 
+                url('hvac/', views.page_hvac, name='page_hvac'), 
+                url('temp/', views.page_temp, name='page_temp'), 
+                url('cool/', views.page_cool, name='page_cool'), 
+                url('elec/'', views.page_elec, name='page_elec'), 
+                url('peak/', views.page_peak, name='page_peak'), 
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
