10653426f5e31efb304c97124a6051ff786a632e
br011503 <49256179+br011503@users.noreply.github.com>
2021-08-25 20:09:36 +0900

    Update views.py

--- hvac/views.py
+++ hvac/views.py
@@ -14,7 +14,30 @@ def allowSelfSignedHttps(allowed):
         ssl._create_default_https_context = ssl._create_unverified_context
 
 def index(request):
-    return render(request, "tables/table1.html")
+
+    allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+    data = {'param': {'bld': '1' }}
+
+    body = str.encode(json.dumps(data))
+
+    url = 'http://52.141.0.146:80/api/v1/service/tsop-skt-borame-main/score'
+    api_key = 'bNxBFk8mDGy4OWxYsu5vlAulYglVUCh1' # Replace this with the API key for the web service
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+    req = urllib.request.Request(url, body, headers)
+
+    try:
+        response = urllib.request.urlopen(req)
+        result = response.read()
+        result = json.loads(result.decode("utf-8"))
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+
+    values = json.loads(result['df1'])
+    values['df1'] = json.loads(result['df1'])
+    values['df2'] = json.loads(result['df2'])
+    values['df3'] = json.loads(result['df3'])
+    return render(request, "tables/table1.html", context = values)
 
 def page_hvac(request):
     return render(request, "tables/page_hvac.html")
